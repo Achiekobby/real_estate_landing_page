4b4fc0b856d3c759284ba8db633da745a0b855ff
Kwabena Nana Acheampong <achiekobby17@gmail.com>
2023-01-18 23:02:19 +0000

    sidebar for smaller screens has been implemented successfully

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import Logo from "../assets/logo.png";
 import {BiMenu} from "react-icons/bi"
 import { Link } from "react-router-dom";
