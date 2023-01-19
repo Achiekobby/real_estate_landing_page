217f56ebe535a5550050ce927b833120859576ab
Kwabena Nana Acheampong <achiekobby17@gmail.com>
2023-01-19 06:51:47 +0000

    The header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
-
+import React from "react";
+import "../scss/Header.scss"
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <section className="header">
+      <div className="header_container">
+        <div className="header_wrapper">
+          <div className="content">
+            <div className="topLine">
+              <h1>
+                <span>New</span> BUILDS AND RENOVATIONS
+              </h1>
+            </div>
+            <div className="headLine">
+              <p>
+                We Supply and install in both new homes and renovations &
+                add_ons
+              </p>
+            </div>
+            <button className="details-btn">Details</button>
+          </div>
+        <div className="socials">
+          <p>Twitter</p>
+          <p>Facebook</p>
+          <p>linkedIn</p>
+        </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Header
+export default Header;
